45c0dd934c4a7c7690b4b83f50ee1cbde1a329bd
Douglas <supertrack69@gmail.com>
2018-10-17 07:18:05 -0400

    first android2

---
+++ app/src/main/java/ung/csci/a3300/anxietymanagement/MainActivity.kt
@@ -0,0 +1,12 @@
+package ung.csci.a3300.anxietymanagement
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
