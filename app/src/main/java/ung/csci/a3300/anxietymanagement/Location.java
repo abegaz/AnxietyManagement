837932266d8d4ee099c26484f10075e558ac5488
josh-noel <joshnoel97@gmail.com>
2018-11-19 00:05:06 -0500

    Added dropdown menus to the screen

--- app/src/main/java/ung/csci/a3300/anxietymanagement/Location.java
+++ app/src/main/java/ung/csci/a3300/anxietymanagement/Location.java
@@ -9,28 +9,28 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.util.concurrent.Executor;
 
 public class Location {
-    public boolean getUserLocation()
-    {
-        FusedLocationProviderClient mFusedLocationClient =  LocationServices.getFusedLocationProviderClient() (this);
-
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-
-                    }
-                })
-                .addOnFailureListener((Executor) this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
-
-        return true;
-
-    }
+//    public boolean getUserLocation()
+//    {
+//        FusedLocationProviderClient mFusedLocationClient =  LocationServices.getFusedLocationProviderClient() (this);
+//
+//        mFusedLocationClient.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        // Got last known location. In some rare situations this can be null.
+//
+//                    }
+//                })
+//                .addOnFailureListener((Executor) this, new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                    }
+//                });
+//
+//        return true;
+//
+//    }
 
 
 }
